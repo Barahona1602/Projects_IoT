52993a797cf35fd4a0310d4cf8ce0487bad3fe72
andre-corado <3007947540101@ingenieria.usac.edu.gt>
2023-08-06 17:45:44 -0600

    Demo Termómetro
    Primer termómetro

--- Practica 1/frontend/sketch.js
+++ Practica 1/frontend/sketch.js
@@ -1,3 +1,8 @@
+// Declarar variables globales (temperatura, humedad, luz)
+var temperatura = 0;
+var humedad = 0;
+var luz = 0;
+
 function setup() {
   createCanvas(400, 400);
   loadJSON('http://127.0.0.1:5000/all', gotData);
@@ -9,5 +14,34 @@ function gotData(data) {
 }
 
 function draw() {
-  background(100);
+  //background white with a tone of gray
+  background(220); 
+  // Draw a thermometer in celsius
+  fill(255, 0, 0); // Red
+  rect(150, 100, 50, 200);
+  fill(200); // Blue  
+  rect(150, 100, 50, 200 - (temperatura*4));
+  // Set title Thermometer
+  fill(0);
+  textSize(20);
+  text("Termómetro", 120, 50);
+  // Set celcius Degress
+  fill(0);
+  textSize(20);
+  text(temperatura + "°C", 160, 330); 
+  // Now draw at the left, small horizontal lines of the thermometer
+  for (var i = 100; i <= 300; i = i + 20) {
+    line(150, i, 160, i);
+  }
+  // Put the amount of celcius degrees
+  textSize(8);
+  for (var i = 100; i <= 300; i = i + 20) {
+    text((300 - i)/4, 120, i);
+  }
+
+
+  
+  
+  
+
 }
